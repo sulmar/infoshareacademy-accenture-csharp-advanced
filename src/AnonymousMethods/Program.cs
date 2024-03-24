833097663f2315b4b89a3ce5dcb87d6429785c6d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-03-24 14:29:15 +0100

    Add AnonymousMethods

---
+++ src/AnonymousMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using Delegates;
+
+Console.WriteLine("Hello, Delegates!");
+
+
+
+Printer printer = new Printer();
+printer.Print("Lorem ipsum");
+printer.Print("Lorem ipsum", 3);
+
+Console.WriteLine();
