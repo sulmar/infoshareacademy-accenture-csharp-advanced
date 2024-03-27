b2f3ae33570f5681a13e6be3ac3e4764189a6f66
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-03-27 13:17:05 +0100

    Użycie WhenAll

--- src/AsynchronousProgramming/Program.cs
+++ src/AsynchronousProgramming/Program.cs
@@ -3,7 +3,9 @@ using AsynchronousProgramming;
 
 Console.WriteLine("Hello, Asynchronous Programming!");
 
-TasksTestAsync();
+MultiTasksTestAsync();
+
+// TasksTestAsync();
 
 Console.WriteLine("Press any key to exit.");
 Console.ReadKey();
@@ -14,7 +16,27 @@ static void DumpThreadId(string message)
     Console.WriteLine($"{message}: Thread ID = {Thread.CurrentThread.ManagedThreadId}");
 }
 
-static async Task TasksTestAsync()
+static async void MultiTasksTestAsync()
+{
+    var productService = new ProductService();
+
+    var price1Task = productService.GetPriceAsync(1);
+    var price2Task = productService.GetPriceAsync(2);
+
+    // Task.WaitAll(price1Task, price2Task);
+
+    await Task.WhenAll(price1Task, price2Task);
+
+    var price1 = price1Task.Result;
+    var price2 = price2Task.Result;
+
+    var total = price1 + price2;
+    Console.WriteLine(total);
+}
+
+
+
+static async void TasksTestAsync()
 {
     var productId = 1;
 
@@ -26,11 +48,19 @@ static async Task TasksTestAsync()
     // decimal conversionRate = currencyConverterService.GetConversionRate("PLN", "EUR");
 
     var price = await productService.GetPriceAsync(productId).ConfigureAwait(false);
+
     decimal conversionRate = await currencyConverterService.GetConversionRateAsync("PLN", "EUR").ConfigureAwait(false);
 
     var priceInEur = price * conversionRate;
 
     Console.WriteLine($"Price in EUR: {priceInEur}");
 
-    logger.Log($"Calculated price in EUR for product {productId}: {priceInEur:C}");
+    try
+    {
+        await logger.LogAsync($"Calculated price in EUR for product {productId}: {priceInEur:C}");
+    }
+    catch (Exception ex)
+    {
+
+    }
 }
