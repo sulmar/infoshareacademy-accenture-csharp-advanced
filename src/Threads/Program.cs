af45c15e520bc5ed30f507a16f091d71735f61a6
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-03-27 09:01:16 +0100

    Dodanie wątków

---
+++ src/Threads/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Diagnostics;
+using System.Net;
+using Threads;
+
+Console.WriteLine("Hello, Threads!");
+
+Stopwatch stopwatch = new Stopwatch();
+
+stopwatch.Start();
+
+for (int i = 0; i < 10; i++)
+{
+    Download("https://picsum.photos/800/600");
+}
+
+stopwatch.Stop();
+
+// Write result.
+Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+
+
+static void Download(string uri)
+{
+    using (var client = new WebClient())
+    {
+        $"Downloading {uri}...".DumpThreadId();
+        
+        string content = client.DownloadString(uri);
+
+        $"Downoladed. {uri}".DumpThreadId();
+
+        
+    }
+}
---
+++ src/Threads/ThreadHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace Threads;
+
+public static class ThreadHelper
+{
+    public static void DumpThreadId(this string message)
+    {
+        Console.WriteLine($"Thread #{Thread.CurrentThread.ManagedThreadId} {message} ");
+    }
+}
